0ba384c8f88c007a0a6e021e85fbb8de59cf971f
Paulo Victor <paulobomtempo25@gmail.com>
2023-10-18 10:49:46 -0300

    Inicio do projeto
    Foi montada a estrutura mínima para começar o desenvolvimento do projeto.

---
+++ GestaoComercial/UIWinFormsApp/FormPrincipal.cs
@@ -0,0 +1,15 @@
+namespace UIWinFormsApp
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
